4655717647a29cea6aaa6bab621d793f63ef630c
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2024-01-13 10:08:16 +0100

    defined the next auth config

---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import db from '@/db';
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+	throw new Error('Missing or invalid github oauth credentials');
+}
+
+const {
+	handlers: { GET, POST },
+	auth,
+	signIn,
+	signOut,
+} = NextAuth({
+	adapter: PrismaAdapter(db),
+	providers: [
+		GitHub({
+			clientId: GITHUB_CLIENT_ID,
+			clientSecret: GITHUB_CLIENT_SECRET,
+		}),
+	],
+	callbacks: {
+		// This session callback is usually not needed. But there is a bug in next-auth atm that requires it
+		async session({ session, user }: any) {
+			if (session && user) {
+				session.user.id = user.id;
+			}
+
+			return session;
+		},
+	},
+});
